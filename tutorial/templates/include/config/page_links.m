7f57be311ce9d03894bb7ae66c2d6593f3e3cb0f
b3m2a1 <b3m2a1@gmail.com>
2019-01-05 18:49:23 -0500

    Added base themes

---
+++ tutorial/templates/include/config/page_links.m
@@ -0,0 +1,18 @@
+{
+  <|
+    "href"->"pages/about.html",
+    "body"->"About"
+    |>,
+  <|
+    "href"->"pages/toc.html",
+    "body"->"Table Of Contents"
+    |>,
+  <|
+    "href"->"categories.html",
+    "body"->"Categories"
+    |>,
+  <|
+    "href"->"tags.html",
+    "body"->"Tags"
+    |>
+  }
